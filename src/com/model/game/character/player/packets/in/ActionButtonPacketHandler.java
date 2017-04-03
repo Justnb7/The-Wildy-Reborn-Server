6d6676f9a18ed4936ba7a6dfa0e5e8809ba32f3a
PATRICKPC\patrick <patrick.vanelderen@live.nl>
2017-04-03 08:38:19 +0200

    no message

--- src/com/model/game/character/player/packets/in/ActionButtonPacketHandler.java
+++ src/com/model/game/character/player/packets/in/ActionButtonPacketHandler.java
@@ -237,7 +237,7 @@ public class ActionButtonPacketHandler implements PacketType {
 		case 140162:
 		case 162235:
 		case 222170:
-		case 29877:
+		case 116181:
 			player.getActionSender().sendRemoveInterfacePacket();
 			break;
 			

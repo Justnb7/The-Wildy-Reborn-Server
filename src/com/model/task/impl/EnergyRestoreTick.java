4a8a1c172a160553a5c5cf71a4ac361a364dd4ac
PATRICKPC\patrick <patrick.vanelderen@live.nl>
2017-06-20 15:40:22 +0200

    Smooth code for walking purposes

---
+++ src/com/model/task/impl/EnergyRestoreTick.java
@@ -0,0 +1,31 @@
+package com.model.task.impl;
+
+import com.model.game.character.Entity;
+import com.model.task.ScheduledTask;
+
+public class EnergyRestoreTick extends ScheduledTask {
+
+	/**
+	 * The mob whos energy we are restoring.
+	 */
+	private Entity entity;
+	
+	public EnergyRestoreTick(Entity entity) {
+		super(4);
+		this.entity = entity;
+	}
+
+	@Override
+	public void execute() {
+		if(entity.getWalkingQueue().getEnergy() < 100) {
+			entity.getWalkingQueue().setEnergy(entity.getWalkingQueue().getEnergy() + 1);
+			if(entity.getActionSender() != null) {
+				entity.getActionSender().sendRunEnergy();
+			}
+		} else {
+			entity.getEnergyRestoreTick().stop();
+			entity.setEnergyRestoreTick(null);
+		}
+	}
+
+}

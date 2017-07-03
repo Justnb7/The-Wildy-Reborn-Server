efd4dee7748dfd03039b7dcf3170ee44d33299d9
PATRICKPC\patrick <patrick.vanelderen@live.nl>
2017-07-03 16:41:09 +0200

    Implemented the Hyperion spell effect classes the effects are unused as of yet.

---
+++ src/com/venenatis/game/model/combat/magic/spell/SpellHandler.java
@@ -0,0 +1,50 @@
+package com.venenatis.game.model.combat.magic.spell;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import com.venenatis.game.model.combat.magic.spell.impl.IceBarrageSpellEffect;
+import com.venenatis.game.model.combat.magic.spell.impl.IceBlitzSpellEffect;
+import com.venenatis.game.model.combat.magic.spell.impl.IceBurstSpellEffect;
+import com.venenatis.game.model.combat.magic.spell.impl.IceRushSpellEffect;
+import com.venenatis.game.model.entity.Entity;
+import com.venenatis.game.model.entity.player.Player;
+
+public class SpellHandler {
+
+	private static Map<Integer, SpellEffect> spells = new HashMap<Integer, SpellEffect>();
+	
+	static {
+		SpellEffect effect = new IceBarrageSpellEffect();
+		spells.put(effect.spellId(), effect);
+		effect = new IceRushSpellEffect();
+		spells.put(effect.spellId(), effect);
+		effect = new IceBurstSpellEffect();
+		spells.put(effect.spellId(), effect);
+		effect = new IceBlitzSpellEffect();
+		spells.put(effect.spellId(), effect);
+		
+	}
+	
+	public static void handleSpellEffect(Player player, Entity victim) {
+		int spellId = player.getSpellId() > -1 ? player.getSpellId() : player.getAutocastId() > -1 ? player.getAutocastId() : -1;
+		SpellEffect effect = forId(spellId);
+		if (effect == null) {
+			return;
+		}
+		if (!effect.noEffect(player, victim))
+			effect.handle(player, victim);
+	}
+	
+	/**
+	 * Returns the spell effect by the id of the spell
+	 * @param id
+	 * The id of the spell
+	 * @return
+	 * The spell effect
+	 */
+	public static SpellEffect forId(int id) {
+		return spells.get(id);
+	}
+
+}
---
+++ src/com/venenatis/game/model/combat/magic/spell/impl/IceBlitzSpellEffect.java
@@ -0,0 +1,23 @@
+package com.venenatis.game.model.combat.magic.spell.impl;
+
+import com.venenatis.game.model.combat.magic.spell.SpellEffect;
+import com.venenatis.game.model.entity.Entity;
+import com.venenatis.game.model.entity.player.Player;
+
+public class IceBlitzSpellEffect implements SpellEffect {
+
+	@Override
+	public void handle(Player attacker, Entity victim) {
+		victim.freeze(15);
+	}
+
+	@Override
+	public boolean noEffect(Player attacker, Entity victim) {
+		return victim.frozen() || (Boolean) victim.getAttribute("frozen_immunity");
+	}
+
+	@Override
+	public int spellId() {
+		return 12871;
+	}
+}

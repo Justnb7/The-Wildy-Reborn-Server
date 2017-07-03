e80dd3499dd5ec7dae6ed61e5dd00b24ace0e2c8
Shadowy <shadow_jak@hotmail.co.uk>
2017-07-03 15:25:35 +0100

    Neaten veng code

--- src/com/venenatis/game/model/combat/magic/lunar/CombatSpells.java
+++ src/com/venenatis/game/model/combat/magic/lunar/CombatSpells.java
@@ -1,79 +1,20 @@
 package com.venenatis.game.model.combat.magic.lunar;
 
-import com.venenatis.game.model.Skills;
 import com.venenatis.game.model.entity.Entity;
-import com.venenatis.game.model.entity.Hit;
-import com.venenatis.game.model.entity.HitType;
-import com.venenatis.game.model.entity.npc.NPC;
 import com.venenatis.game.model.entity.player.Player;
-import com.venenatis.game.task.Task;
-import com.venenatis.server.Server;
 
 public class CombatSpells {
 	
 	public static void vengeance(Player venger, Entity entity_attacker, final int damage, int delay) {
-		
-		/**
+		/*
 		 * Minimum hit required
 		 */
-		if (damage < 2) {
+		if (damage < 2 || !venger.hasVengeance()) {
 			return;
 		}
-
-		/**
-		 * The player entity
-		 */
-		if (entity_attacker instanceof Player) {
-			Player attacker = (Player) entity_attacker;
-			if(!venger.hasVengeance()) {
-				return;
-			}
-			
-			Server.getTaskScheduler().schedule(new Task(delay) {
-				@Override
-				public void execute() {
-					if (damage < 2) {
-						return;
-					}
-					int hit = (int) (damage * 0.75);
-					if (hit < 1) {
-						return;
-					}
-					if (attacker.getCombatState().isDead()) {
-						hit = 0;
-					}
-					venger.sendForcedMessage("Taste vengeance!");
-					venger.setVengeance(false);
-					attacker.damage(new Hit(hit > attacker.getSkills().getLevel(Skills.HITPOINTS) ? attacker.getSkills().getLevel(Skills.HITPOINTS) : hit));
-					this.stop();
-				}
-			});
-			/**
-			 * The npc entity
-			 */
-		} else {
-			NPC attacker_npc = (NPC) entity_attacker;
-			
-			Server.getTaskScheduler().schedule(new Task(delay) {
-				@Override
-				public void execute() {
-					if (damage < 2) {
-						return;
-					}
-					int hit = (int) (damage * 0.75);
-					if (hit < 1) {
-						return;
-					}
-					if (attacker_npc.getCombatState().isDead()) {
-						hit = 0;
-					}
-					venger.sendForcedMessage("Taste vengeance!");
-					venger.setVengeance(false);
-					attacker_npc.damage(new Hit(damage, damage > 0 ? HitType.NORMAL : HitType.BLOCKED));
-					this.stop();
-				}
-			});
-		}
+		venger.sendForcedMessage("Taste vengeance!");
+		venger.setVengeance(false);
+		entity_attacker.take_hit(venger, (int)(damage*.75), null).send(delay); // no combat xp given from veng damage
 	}
 
 }

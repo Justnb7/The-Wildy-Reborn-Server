512a19cd47d739fdc38c4099c5a350422a77d041
PATRICKPC\patrick <patrick.vanelderen@live.nl>
2017-09-09 20:22:05 +0200

    Client update.

--- src/com/venenatis/game/constants/Constants.java
+++ src/com/venenatis/game/constants/Constants.java
@@ -64,7 +64,7 @@ public class Constants {
 	 * The current version of the client. Used to notify player to update
 	 * client.
 	 */
-	public static final int CLIENT_VERSION = 11;
+	public static final int CLIENT_VERSION = 12;
 
 	/**
 	 * The name of the server
